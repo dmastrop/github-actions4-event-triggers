8f08be1a5fd2647ca2c4fd00bcabdc88e2a4bb82
IdaSa2 <135462973+IdaSa2@users.noreply.github.com>
2023-06-05 16:13:03 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+//IdaSa2 edit of main.jsx on fork master
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
